66a05913d638c3d6bffe5df43758b1db66168d98
vakur1 <temavakurov2@icloud.com>
2022-12-08 23:08:08 +0300

    data слой

--- app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/example/shoppinglist/domain/ShopItem.kt
@@ -1,9 +1,13 @@
 package com.example.shoppinglist.domain
 
 data class ShopItem(
-    val id : Int,
     val name : String,
     val count : Int,
-    val enable : Boolean
-)
+    val enable : Boolean,
+    var id : Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
 
