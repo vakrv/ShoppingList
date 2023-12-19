832ad3f76e789172cf02631525a0e8de0324436d
vakur1 <temavakurov2@icloud.com>
2023-12-19 22:45:37 +0300

    Создание ShopItemViewModel

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,67 @@
+package com.example.shoppinglist.presentation
+
+import androidx.lifecycle.ViewModel
+import com.example.shoppinglist.data.ShopListRepositoryImpl
+import com.example.shoppinglist.domain.AddShopItemUseCase
+import com.example.shoppinglist.domain.EditShopItemUseCase
+import com.example.shoppinglist.domain.GetShopItemUseCase
+import com.example.shoppinglist.domain.ShopItem
+
+class ShopItemViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+        }
+
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
+            editShopItemUseCase.editShopItem(shopItem)
+        }
+
+    }
+
+    private fun parseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            return 0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            //TODO show error input name
+            result = false
+        }
+        if(count <= 0) {
+            //TODO show error input count
+            result = false
+        }
+        return result
+    }
+}
